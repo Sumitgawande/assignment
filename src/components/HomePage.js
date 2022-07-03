542394195c2385d0389b4fdbe4423613b4eaa822
Sumit_Gawande <gawandesumit97@gmail.com>
2022-07-03 08:34:17 +0530

    fresh commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -28,17 +28,12 @@ console.log('new data added');
 
 
 useEffect(()=>{
-  console.log('new mwssdhslkjsk');
-  console.log('new mwssdhslkjsk');
-  console.log('new mwssdhslkjsk');
-  console.log('new mwssdhslkjsk');
-  console.log('new mwssdhslkjsk');
-  console.log('new mwssdhslkjsk');
+  
   console.log('new master data');
   console.log('new master data');
   console.log('new master data');
   console.log('new master data');
-  console.log('new mwssdhslkjsk');
+  
   if(value === 0){
     setName('Rick');
   }else{
